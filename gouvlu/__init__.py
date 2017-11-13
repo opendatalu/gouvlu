238e5938b4748e174e411187e6e1c53ee0aa9809
Axel Haustant <noirbizarre@gmail.com>
2017-11-13 10:55:29 +0100

    Move theme into a subpackage

--- gouvlu/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-'''
-gouvlu
-
-Official udata theme of the Open Data Portal of Luxembourg
-'''
-from __future__ import unicode_literals
---
+++ gouvlu/theme/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+'''
+gouvlu
+
+Official udata theme of the Open Data Portal of Luxembourg
+'''
+from __future__ import unicode_literals
