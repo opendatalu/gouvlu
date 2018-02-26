b0f689f253f4e786376ec574dfd6b3c93ffd225b
Patrick Weber <pweber@spatialbit.com>
2018-02-26 15:38:39 +0100

    Bump version 0.1.6

--- gouvlu/__init__.py
+++ gouvlu/__init__.py
@@ -4,5 +4,5 @@ Official udata theme and extensions of the Open Data Portal of Luxembourg
 '''
 from __future__ import unicode_literals
 
-__version__ = '0.1.6.dev'
+__version__ = '0.1.6'
 __description__ = 'Official udata theme and extensions of the Open Data Portal of Luxembourg'
