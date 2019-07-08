bf80b66c5fc4510a4b3172a9b30e5cc58457f76c
Joe Mayer <lsb257@Mayers-Mac-mini.local>
2019-07-08 14:25:01 +0200

    Update to version 1.1.15.dev for next development cycle

--- gouvlu/__init__.py
+++ gouvlu/__init__.py
@@ -4,5 +4,5 @@ Official udata theme and extensions of the Open Data Portal of Luxembourg
 '''
 from __future__ import unicode_literals
 
-__version__ = '1.1.14'
+__version__ = '1.1.15.dev'
 __description__ = 'Official udata theme and extensions of the Open Data Portal of Luxembourg'
