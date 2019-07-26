c6d344e5571b06f8b688be200c126d37d56868b4
Joe Mayer <lsb257@Mayers-Mac-mini.local>
2019-07-26 09:21:45 +0200

    Bump version 1.1.55

--- gouvlu/__init__.py
+++ gouvlu/__init__.py
@@ -4,5 +4,5 @@ Official udata theme and extensions of the Open Data Portal of Luxembourg
 '''
 from __future__ import unicode_literals
 
-__version__ = '1.1.55.dev'
+__version__ = '1.1.55'
 __description__ = 'Official udata theme and extensions of the Open Data Portal of Luxembourg'
